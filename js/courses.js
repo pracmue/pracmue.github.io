f12bafdb3a3684e8f5f35f9b67f7c200442e4993
Prachya M. <pracmue@gmail.com>
2026-07-11 15:08:59 +0700

    Add files via upload

---
+++ js/courses.js
@@ -0,0 +1,70 @@
+const courses = [
+
+    {
+        code: "IN 542002",
+        title: "Macroeconomics II",
+        image: "assets/icon/542002.png",
+        password: "001",
+        description: "Lecture Notes, Learning Materials and Assignments.",
+        weeks: [
+            {lecture:{title:"Lecture 1", url:"assets/542002/Ch-1.pdf"}, video:{title:"Video 1",url:"https://youtube.com/..."},exercise:{title:"Exercise 1", url:"assets/542002/ex1.pdf"}},
+            {lecture:{title:"Lecture 2", url:"assets/542002/Ch-2.pdf"}, video:{title:"Video 2",url:"https://youtube.com/..."},exercise:{title:"Exercise 2", url:"assets/542002/ex2.pdf"}},
+            {lecture:{title:"Lecture 3", url:"assets/542002/Ch-3.pdf"}, video:{title:"Video 3",url:"https://youtube.com/..."},exercise:{title:"Exercise 3", url:"assets/542002/ex3.pdf"}},
+            {lecture:{title:"Lecture 4", url:"assets/542002/Ch-4.pdf"}, video:{title:"Video 4",url:"https://youtube.com/..."},exercise:{title:"Exercise 4", url:"assets/542002/ex4.pdf"}},
+            {lecture:{title:"Lecture 5", url:"assets/542002/Ch-5.pdf"}, video:{title:"Video 5",url:"https://youtube.com/..."},exercise:{title:"Exercise 5", url:"assets/542002/ex5.pdf"}},
+            {lecture:{title:"Lecture 6", url:"assets/542002/Ch-6.pdf"}, video:{title:"Video 6",url:"https://youtube.com/..."},exercise:{title:"Exercise 6", url:"assets/542002/ex6.pdf"}},
+            {lecture:{title:"Lecture 7", url:"assets/542002/Ch-7.pdf"}, video:{title:"Video 7",url:"https://youtube.com/..."},exercise:{title:"Exercise 7", url:"assets/542002/ex7.pdf"}},
+            {lecture:{title:"Lecture 8", url:"assets/542002/Ch-8.pdf"}, video:{title:"Video 8",url:"https://youtube.com/..."},exercise:{title:"Exercise 8", url:"assets/542002/ex8.pdf"}},
+            {lecture:{title:"Lecture 9", url:"assets/542002/Ch-9.pdf"}, video:{title:"Video 9",url:"https://youtube.com/..."},exercise:{title:"Exercise 9", url:"assets/542002/ex9.pdf"}},
+            {lecture:{title:"Lecture 10", url:"assets/542002/Ch-10.pdf"}, video:{title:"Video 10",url:"https://youtube.com/..."},exercise:{title:"Exercise 10", url:"assets/542002/ex10.pdf"}}
+            ],
+        extraCard: {title: "📢 Announcement", text: "Midterm Exam : ... August 2026", button: "Schedule", url: "assets/542002/midterm.pdf"}},
+
+
+
+    {
+        code: "IN 542003",
+        title: "Introduction to Statatic for Economists",
+        image: "assets/icon/542003.png",
+        password: "002",
+        description: "Lecture Notes, Learning Materials and Assignments.",
+        weeks: [
+            {lecture:{title:"Lecture 1", url:"assets/542003/Ch-1.pdf"}, video:{title:"Video 1",url:"https://youtube.com/..."},exercise:{title:"Exercise 1", url:"assets/542003/ex1.pdf"}},
+            {lecture:{title:"Lecture 2", url:"assets/542003/Ch-2.pdf"}, video:{title:"Video 2",url:"https://youtube.com/..."},exercise:{title:"Exercise 2", url:"assets/542003/ex2.pdf"}},
+            {lecture:{title:"Lecture 3", url:"assets/542003/Ch-3.pdf"}, video:{title:"Video 3",url:"https://youtube.com/..."},exercise:{title:"Exercise 3", url:"assets/542003/ex3.pdf"}},
+            {lecture:{title:"Lecture 4", url:"assets/542003/Ch-4.pdf"}, video:{title:"Video 4",url:"https://youtube.com/..."},exercise:{title:"Exercise 4", url:"assets/542003/ex4.pdf"}},
+            {lecture:{title:"Lecture 5", url:"assets/542003/Ch-5.pdf"}, video:{title:"Video 5",url:"https://youtube.com/..."},exercise:{title:"Exercise 5", url:"assets/542003/ex5.pdf"}},
+            {lecture:{title:"Lecture 6", url:"assets/542003/Ch-6.pdf"}, video:{title:"Video 6",url:"https://youtube.com/..."},exercise:{title:"Exercise 6", url:"assets/542003/ex6.pdf"}},
+            {lecture:{title:"Lecture 7", url:"assets/542003/Ch-7.pdf"}, video:{title:"Video 7",url:"https://youtube.com/..."},exercise:{title:"Exercise 7", url:"assets/542003/ex7.pdf"}},
+            {lecture:{title:"Lecture 8", url:"assets/542003/Ch-8.pdf"}, video:{title:"Video 8",url:"https://youtube.com/..."},exercise:{title:"Exercise 8", url:"assets/542003/ex8.pdf"}},
+            {lecture:{title:"Lecture 9", url:"assets/542003/Ch-9.pdf"}, video:{title:"Video 9",url:"https://youtube.com/..."},exercise:{title:"Exercise 9", url:"assets/542003/ex9.pdf"}},
+            {lecture:{title:"Lecture 10", url:"assets/542003/Ch-10.pdf"}, video:{title:"Video 10",url:"https://youtube.com/..."},exercise:{title:"Exercise 10", url:"assets/542003/ex10.pdf"}}
+            ],
+        extraCard: {title: "📢 Announcement", text: "Midterm Exam : ... August 2026", button: "Schedule", url: "assets/542003/midterm.pdf"}},
+
+
+
+
+    {
+        code: "IN 544002",
+        title: "Derivative Market",
+        image: "assets/icon/544002.png",
+        password: "econ",
+        description: "Lecture Notes, Learning Materials and Assignments.",
+        weeks: [
+            {lecture:{title:"Lecture 1", url:"assets/544002/Ch-1.pdf"}, video:{title:"Video 1",url:"https://youtube.com/..."},exercise:{title:"Exercise 1", url:"assets/544002/ex1.pdf"}},
+            {lecture:{title:"Lecture 2", url:"assets/544002/Ch-2.pdf"}, video:{title:"Video 2",url:"https://youtube.com/..."},exercise:{title:"Exercise 2", url:"assets/544002/ex2.pdf"}},
+            {lecture:{title:"Lecture 3", url:"assets/544002/Ch-3.pdf"}, video:{title:"Video 3",url:"https://youtube.com/..."},exercise:{title:"Exercise 3", url:"assets/544002/ex3.pdf"}},
+            {lecture:{title:"Lecture 4", url:"assets/544002/Ch-4.pdf"}, video:{title:"Video 4",url:"https://youtube.com/..."},exercise:{title:"Exercise 4", url:"assets/544002/ex4.pdf"}},
+            {lecture:{title:"Lecture 5", url:"assets/544002/Ch-5.pdf"}, video:{title:"Video 5",url:"https://youtube.com/..."},exercise:{title:"Exercise 5", url:"assets/544002/ex5.pdf"}},
+            {lecture:{title:"Lecture 6", url:"assets/544002/Ch-6.pdf"}, video:{title:"Video 6",url:"https://youtube.com/..."},exercise:{title:"Exercise 6", url:"assets/544002/ex6.pdf"}},
+            {lecture:{title:"Lecture 7", url:"assets/544002/Ch-7.pdf"}, video:{title:"Video 7",url:"https://youtube.com/..."},exercise:{title:"Exercise 7", url:"assets/544002/ex7.pdf"}},
+            {lecture:{title:"Lecture 8", url:"assets/544002/Ch-8.pdf"}, video:{title:"Video 8",url:"https://youtube.com/..."},exercise:{title:"Exercise 8", url:"assets/544002/ex8.pdf"}},
+            {lecture:{title:"Lecture 9", url:"assets/544002/Ch-9.pdf"}, video:{title:"Video 9",url:"https://youtube.com/..."},exercise:{title:"Exercise 9", url:"assets/544002/ex9.pdf"}},
+            {lecture:{title:"Lecture 10", url:"assets/544002/Ch-10.pdf"}, video:{title:"Video 10",url:"https://youtube.com/..."},exercise:{title:"Exercise 10", url:"assets/544002/ex10.pdf"}}
+            ],
+        extraCard: {title: "📢 Announcement", text: "Midterm Exam : ... August 2026", button: "Schedule", url: "assets/544002/midterm.pdf"}}
+
+
+
+];
